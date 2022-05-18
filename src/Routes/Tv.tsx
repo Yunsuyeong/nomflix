25492e0ae449f650f33050eaf54cbc0c3e0da75f
Yunsuyeong <vanquishr12@gmail.com>
2022-05-19 00:30:54 +0900

    2022-05-19(Tv 화면)

--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { motion, AnimatePresence, useViewportScroll } from "framer-motion";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import { makeImagePath } from "../utilis";
+import { getOnairTvs, IGetOnAirTvResult } from "../api";
 
 const Wrapper = styled.div`
     background: #95a5a6;
@@ -12,10 +18,186 @@ const Loader = styled.div`
     align-items: center;
 `;
 
+const Banner = styled.div<{ bgPhoto: string }>`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 60px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+        url(${(props) => props.bgPhoto});
+    background-size: cover;
+`;
+
+const Title = styled.h2`
+    font-size: 48px;
+    margin-bottom: 120px;
+`;
+
+const Overview = styled.p`
+    position: relative;
+    top: -120px;
+    font-size: 18px;
+    width: 50%;
+`;
+
+const Slider = styled.div`
+    position: relative;
+    top: -510px;
+    right: -1120px;
+`;
+
+const Column = styled(motion.div)`
+    display: grid;
+    gap: 5px;
+    grid-template-rows: repeat(6, 1fr);
+    position: absolute;
+    width: 10%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+    position: relative;
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    height: 80px;
+    font-size: 66px;
+    cursor: pointer;
+`;
+
+const Info = styled(motion.div)`
+    background-color: ${(props) => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    h4 {
+        text-align: center;
+        font-size: 12px;
+    }
+`;
+
+const colVariants = {
+    invisible: {
+        y: window.outerHeight + 5,
+    },
+    visible: {
+        y: 0,
+    },
+    exit: {
+        y: -window.outerHeight - 5,
+    },
+};
+
+const boxVariants = {
+    normal: {
+        scale: 1,
+    },
+    hover: {
+        zIndex: 99,
+        scale: 1.3,
+        y: -50,
+        transition: {
+            delay: 0.5,
+            duration: 0.3,
+            type: "tween",
+        },
+    },
+};
+
+const infoVariants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.3,
+            type: "tween",
+        },
+    },
+};
+
+const offset = 6;
+
 function Tv() {
+    const history = useHistory();
+    const { scrollY } = useViewportScroll();
+    const { data, isLoading } = useQuery<IGetOnAirTvResult>(
+        ["tvs", "onair"],
+        getOnairTvs
+    );
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const toggleLeaving = () => setLeaving((prev) => !prev);
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalTvs = data.results.length - 1;
+            const maxIndex = Math.floor(totalTvs / offset) - 1;
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }
+    };
+    const onBoxClicked = (tvid: string) => {
+        history.push(`/tvs/${tvid}`);
+    };
     return (
         <Wrapper>
-            <Loader>Loading...</Loader>
+            {isLoading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Banner
+                        bgPhoto={makeImagePath(
+                            data?.results[0].backdrop_path || ""
+                        )}
+                    >
+                        <Title>{data?.results[0].original_name}</Title>
+                        <Overview>{data?.results[0].overview}</Overview>
+                    </Banner>
+                    <Slider onClick={increaseIndex}>
+                        <AnimatePresence
+                            initial={false}
+                            onExitComplete={toggleLeaving}
+                        >
+                            <Column
+                                variants={colVariants}
+                                initial="invisible"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {data?.results
+                                    .slice(1)
+                                    .slice(
+                                        offset * index,
+                                        offset * index + offset
+                                    )
+                                    .map((tv) => (
+                                        <Box
+                                            layoutId={tv.id + ""}
+                                            key={tv.id}
+                                            onClick={() => onBoxClicked}
+                                            bgPhoto={makeImagePath(
+                                                tv.backdrop_path,
+                                                "w500"
+                                            )}
+                                            variants={boxVariants}
+                                            initial="normal"
+                                            whileHover="hover"
+                                            transition={{ type: "tween" }}
+                                        >
+                                            <Info variants={infoVariants}>
+                                                <h4>{tv.original_name}</h4>
+                                            </Info>
+                                        </Box>
+                                    ))}
+                            </Column>
+                        </AnimatePresence>
+                    </Slider>
+                </>
+            )}
         </Wrapper>
     );
 }
--- src/api.ts
+++ src/api.ts
@@ -24,6 +24,22 @@ interface IMovie {
     overview: string;
 }
 
+export interface IGetOnAirTvResult {
+    page: number;
+    results: ITv[];
+    total_pages: number;
+    total_results: number;
+}
+
+interface ITv {
+    backdrop_path: string;
+    poster_path: string;
+    id: number;
+    original_name: string;
+    overview: string;
+    vote_average: number;
+}
+
 export async function getNowMovies() {
     return await fetch(
         `https://api.themoviedb.org/3/movie/now_playing?api_key=9633d3074eb0813143d2c4cf83a6046b`
@@ -33,5 +49,11 @@ export async function getNowMovies() {
 export async function getTopMovies() {
     return await fetch(
         `https://api.themoviedb.org/3/movie/top_rated?api_key=9633d3074eb0813143d2c4cf83a6046b`
+    ).then((response) => response.json());
+}
+
+export async function getOnairTvs() {
+    return await fetch(
+        `https://api.themoviedb.org/3/tv/on_the_air?api_key=9633d3074eb0813143d2c4cf83a6046b`
     ).then((response) => response.json());
 }
